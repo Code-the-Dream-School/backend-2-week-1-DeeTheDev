0574eba3139e4a27af8ff3ad48009ea51bad8228
Denilson V <deethedev@gmail.com>
2020-11-23 18:14:23 -0500

    Update to CRUD operations

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,43 @@
+class PagesController < ApplicationController
+
+    def index
+        @pages = Page.all
+    end
+
+    def show
+        # @page = Page.find(params[:id])
+        # render text: @page.title
+        @page = Page.find(params[:id])
+    end
+
+    def new
+        @page = Page.new
+    end
+
+    def create
+        # @page = Page.new(params)
+        # render text: params.to_json
+        # render text: params.class
+        @page = Page.new(page_params)
+        @page.save
+        redirect_to @page
+    end
+    
+    def edit
+        @page = Page.find(params[:id])
+    end
+
+    def update 
+        @page = Page.find(params[:id])
+        @page.update(page_params)
+        redirect_to @page
+    end
+
+    # Private allows no one else to access this code
+    private
+
+    def page_params
+        params.require(:page).permit(:title, :body, :slug)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
+  get '/pages', to: 'pages#index'
+  post '/pages', to: 'pages#create'
+  get '/pages/new', to: 'pages#new', as: 'new_page'
+  # Change params to bottom to fix priority over the other paths
+  get '/pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  patch '/pages/:id', to: 'pages#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
